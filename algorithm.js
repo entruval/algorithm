6daac4b184f3cd1916eb1346b7aea954de6a30b1
Henry <entruval@gmail.com>
2021-06-23 22:55:52 +0700

    (feat) main notes

---
+++ algorithm.js
@@ -0,0 +1,47 @@
+[list]
+  O(1) - constant
+  O(n) - linear
+  O(n ** 2) - quadratic
+  O(2 ** n) - exponential
+  O(n ** n) - exponential
+  O(n!) - factorial
+  O(log n) - logarthmic
+
+
+[examples]
+  O(1)
+    const s = 0
+
+  O(n)
+    for (let i = 0; i < n; i++) {}
+
+  O(n ** 2)
+
+    O(n)
+      O(n)
+    for (let i = 0; i < x; i++) {
+      for (let j = 0; j < y; j++) {}
+    }
+
+  O(log n)
+    while (n > 0) {
+      n /= 2
+    }
+
+  O(n log n)
+
+    O(n)
+      O(log n)
+    for (let i = 0; i < n; i++) {
+      while (n > 0) {
+        n /= 2
+      }
+    }
+
+  O(n!)
+    function filter(n) {
+      for (i = 0; i < n; i++) {
+        filter(n-1)
+      }
+    }
+
